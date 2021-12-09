87054a953662ddc2bdc4cc27c976737b3b51303e
DGMelca <87624062+DGMelca@users.noreply.github.com>
2021-12-08 23:41:53 -0300

    Entrega proyecto 2

--- js/main.js
+++ js/main.js
@@ -39,9 +39,71 @@ let listaDeProductos = [
     }
 ]
 
-console.log(listaDeProductos);
 
 listaDeProductos.push(new producto("Cepillo de bambú", "Desenredante, no genera frizz", 800, 50));
+
+let acumulador = ``;
+listaDeProductos.forEach((producto) => {
+    acumulador +=  `<div class="col mb-3">
+                        <div class="card h-100 row m-10 p-2">
+                            <img class="card-img-top" src="" alt=""/>
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <h5 class="card-title fw-bolder">${producto.nombre}</h5>
+                                    <p class="card-text">${producto.descripcion}</p>
+                                    <p class="card-price">$${producto.precio}</p>
+                                </div>
+                            </div>
+                            <div class="card-footer border-2">
+                                <div class="text-center mt-auto">
+                                <button class="btn border border-3" id="btnComprar" onclick="agregarAlCarrito('${producto.nombre}');">Agregar al carrito</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`
+});
+
+document.getElementById("galeria").innerHTML = acumulador;
+
+
+let carrito = [];
+if(carrito != null){
+    document.getElementById("contador").innerHTML = carrito.length;
+    carrito = JSON.parse(localStorage.carrito);
+}
+
+function agregarAlCarrito(nombre){
+    const item = listaDeProductos.find(producto => producto.nombre === nombre);
+    if(item != undefined){
+        carrito.push(item);
+    } else{
+        alert("Error");
+    }
+    localStorage.carrito = JSON.stringify(carrito);
+    document.getElementById("contador").innerHTML = carrito.length;
+}
+
+let contCarrito = document.getelementById("contCarrito");
+
+for (const producto of carrito) {
+    let modal = document.createElement("p");
+    modal.innerHTML = `${producto.nombre}`;
+    document.body.appendChild(modal);
+}
+
+
+
+
+/*
+let btnCarrito = document.getElementById("carrito");
+btnCarrito.onclick = () => {console.log(carrito)};
+*/
+
+
+
+
+//////////////// FUNCIONES 
+/*
 
 // Carrito de compras
 
@@ -94,8 +156,8 @@ financiacion = calcularCuotas(precioPromo,cuotas);
     alert("La opción ingresada no es válida");
 }
 
+*/
 
-// VER ¿se podría hacer un array de objeto para los medios de pago? 
 
 
 
